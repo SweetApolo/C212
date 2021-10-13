4c5d6b054e2d3ff3f1f0550931edaa532c24e194
JOJO <boovanegas@gmail.com>
2021-10-13 11:53:48 -0500

    Preprocesador con las intrucciones

---
+++ CP3.c
@@ -0,0 +1,5 @@
+//Preprocesador con las librerias necesarias
+#define Pro(numeroDatos, valoresDatos) valoresDatos/numeroDatos //Definimos la funcion para el Promedio 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
