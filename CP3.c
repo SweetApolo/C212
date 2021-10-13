84e5a7baa7b3c8113979a044d2a503ac359615f5
JOJO <boovanegas@gmail.com>
2021-10-13 12:01:44 -0500

    Codigo token + Acceso al archivo de numeros.txt

--- CP3.c
+++ CP3.c
@@ -3,3 +3,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+int size = 20;
+
+int main(){
+    FILE *fuente = fopen("numeros.txt", "r"); //Flujo para acceder al archivo donde tenemos los numeros
+    //Uso del codigo TOKEN que nos dio el profe Franco
+    char numeros[size]; //Arreglo char con el size definido
+    fgets(numeros, size, fuente); //Los numeros del archivo van a ser los valores del arreglo
+    char *token = strtok(numeros, " "); //Espacio para dividir el arreglo
+    int *arr = malloc(sizeof(int) * size); //Arreglo en memoria dinamica
+    int numeroDatos = 0; //Numeros que tiene el archivo de numeros.txt (6)
+
+    while (token != NULL){
+        sscanf(token, "%d", &arr[numeroDatos]); //Utilizando el ejercicio 6 (Caracteres a numeros)
+        token = strtok(NULL, " ");
+        numeroDatos++;
+    }
+    
+    
+
+}
